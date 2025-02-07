aca54e290347e048ca3ed0bee97af69af23569fe
Moska002 <ale.moska002@gmail.com>
2025-02-07 21:22:01 +0100

    UPDATE New notebook

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
-import numpy
+def get_glass_height_mm(type):
+    if(type == 'A'): return 83.0
+    elif(type == 'B' or type == 'C'): return 88.0
+    else: raise ValueError("This type does not exists")
 
 def get_glass_height_cm(type):
     if(type == 'A'): return 8.30
@@ -8,16 +11,12 @@ def get_glass_height_cm(type):
 def center_crop(image, crop_width, crop_height):
     height, width, _ = image.shape
 
-    # Calculate the center of the image
     center_x, center_y = width // 2, height // 2
 
-    # Define the coordinates for the crop (top-left corner of the ROI)
     x1 = center_x - crop_width // 2
     y1 = center_y - crop_height // 2
 
-    # Define the coordinates for the bottom-right corner of the ROI
     x2 = center_x + crop_width // 2
     y2 = center_y + crop_height // 2
 
-    # Crop the image using the calculated coordinates
     return image[y1:y2, x1:x2]
